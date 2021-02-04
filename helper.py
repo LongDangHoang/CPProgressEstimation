119c9c0edc3ca9c59dcca388c685443618509218
LongDang <hoanglongdang2001@gmail.com>
2021-02-04 14:17:51 +1100

    Add naive version of tree weight, quite slow atm

--- helper.py
+++ helper.py
@@ -2,4 +2,51 @@
 helper file
 """
 
+import sqlite3
+import pandas as pd
+import unittest
 
+
+def to_df(file: str, table: str) -> pd.DataFrame:
+    
+    """
+    Return pandas' dataframe representation of a table in sqlite file (tree search log)
+
+    Parameters:
+        - file: string of the path to the sqlite3 database
+        - table: table to be saved  
+    Returns:
+        - list of dataframes, one for each table in the database
+
+    """
+
+    assert table.upper() in  {'NODES', 'INFO', 'BOOKMARKS', 'NOGOODS'}
+    conn = sqlite3.connect(file)
+    query = 'SELECT * FROM {}'.format(table.upper())
+    return pd.read_sql_query(query, conn)
+
+
+###################
+#### UNIT TEST ####
+###################
+
+class TestHelperMethods(unittest.TestCase):
+
+    def test_todict(self):
+        k = to_df('./trees/golomb_4.sqlite', 'nodes')
+        self.assertEqual(k.loc[0, 'NodeID'], 0)
+        self.assertEqual(k.shape[0], 36)
+        self.assertTrue(all([k.loc[idx, 'NodeID'] == idx for idx in range(k.shape[0])]))
+        self.assertEqual(k.loc[26, 'Status'], 0)
+        self.assertEqual(
+            {'NodeID', 'Status', 'NKids', 'ParentID', 'Alternative', 'Label'},
+            set(k.columns)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+    
--- tree_weight.py
+++ tree_weight.py
@@ -1,11 +1,49 @@
 """
 Implements an offline tree-weight estimation or a constrained search tree
 
-1. Loads tree information as a json object { {node_id, node_parent, etc. } } where node_id correlates
-        with the order the node is visited
-2. Assigns weight = 1 to root and propogates that weight down
-3. Apply weighting scheme at every split
 4. Measure weight as we go along node_id and compare it with actual number of nodes we eventually visit
-
-
 """
+
+import pandas as pd
+from helper import to_df
+
+def uniform_scheme(node_id: int, nodes_df: pd.DataFrame) -> float:
+    """ Assign uniform weight """
+    return 1 / nodes_df.loc[nodes_df.loc[node_id, 'ParentID'], 'NKids']
+
+def assign_weights(nodes_df: pd.DataFrame, weight_scheme: 'function') -> None:
+    """
+    Assigns weight = 1 to root and propogates that weight down using a weighting scheme
+    
+    Parameter:
+        - nodes_df: dataframe of nodes, indexed by nodes_id
+        - weight_scheme: function that takes in the current node_id
+                         and outputs the weight it has relative to its parent node
+    """
+    
+    nodes_df['NodeWeight'] = 0
+    nodes_df.loc[0, 'NodeWeight'] = 1 # root node has weight 1
+    next_ids = nodes_df[nodes_df['ParentID'] == 0].index
+
+    # propogate weights down
+    while len(next_ids) > 0:
+        node = next_ids.pop(0)
+        nodes_df.loc[node, 'NodeWeight'] = nodes_df.loc[nodes_df.loc[node, 'ParentID'], :]\
+                                            ['NodeWeight'] * weight_scheme(node, nodes_df)
+        next_ids = pd.concat(next_ids, nodes_df[nodes_df['ParentID'] == node].index.to_list())
+
+    ## debug:
+    print(nodes_df[['NodeID', 'NodeWeight']])
+    print("Sum of all leaves weights: ", nodes_df[nodes_df['status'].isin({1, 0, 3})]['NodeWeight'].sum())
+
+    # undo all alter table
+    return None
+
+if __name__ == '__main__':
+    ### do testing here
+
+    tree = './trees/golomb_3.sqlite'
+    nodes_df = to_df(tree, 'nodes').set_index('NodeID')
+    
+
+
