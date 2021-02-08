5838f55b4966cfcd2c72f4f193dd6fa1de5a6954
LongDang <hoanglongdang2001@gmail.com>
2021-02-08 12:35:39 +1100

    Add dfs ordering Add correct uniform weighting result

--- tree_weight.py
+++ tree_weight.py
@@ -5,11 +5,12 @@ Implements an offline tree-weight estimation or a constrained search tree
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 from helper import to_df
 
 def uniform_scheme(node_id: int, nodes_df: pd.DataFrame) -> float:
-    """ Assign uniform weight """
-    return 1 / nodes_df.loc[nodes_df.loc[node_id, 'ParentID'], 'NKids']
+    """ Assign uniform weights """
+    return [1 / nodes_df.loc[node_id, 'NKids'] for i in range(nodes_df.loc[node_id, 'NKids'])]
 
 def assign_weights(nodes_df: pd.DataFrame, weight_scheme: 'function') -> None:
     """
@@ -18,32 +19,88 @@ def assign_weights(nodes_df: pd.DataFrame, weight_scheme: 'function') -> None:
     Parameter:
         - nodes_df: dataframe of nodes, indexed by nodes_id
         - weight_scheme: function that takes in the current node_id
-                         and outputs the weight it has relative to its parent node
+                         and outputs a list of weights for its children
     """
     
     nodes_df['NodeWeight'] = 0
     nodes_df.loc[0, 'NodeWeight'] = 1 # root node has weight 1
-    next_ids = nodes_df[nodes_df['ParentID'] == 0].index
+    i = 0
 
     # propogate weights down
-    while len(next_ids) > 0:
-        node = next_ids.pop(0)
-        nodes_df.loc[node, 'NodeWeight'] = nodes_df.loc[nodes_df.loc[node, 'ParentID'], :]\
-                                            ['NodeWeight'] * weight_scheme(node, nodes_df)
-        next_ids = pd.concat(next_ids, nodes_df[nodes_df['ParentID'] == node].index.to_list())
+    while i < nodes_df.shape[0] - 1:
 
-    ## debug:
-    print(nodes_df[['NodeID', 'NodeWeight']])
-    print("Sum of all leaves weights: ", nodes_df[nodes_df['status'].isin({1, 0, 3})]['NodeWeight'].sum())
+        par_weight = nodes_df.loc[i, 'NodeWeight']
+        num_kids = nodes_df.loc[i, 'NKids']
+        if num_kids > 0:
+            weights = uniform_scheme(i, nodes_df)
+            assert len(weights) == num_kids
+            nodes_df.loc[nodes_df['ParentID'] == i, 'NodeWeight'] = par_weight * \
+                                   nodes_df[nodes_df['ParentID'] == i]['Alternative'].apply(lambda x: weights[x]) 
+        i += 1
 
-    # undo all alter table
-    return None
+def make_dfs_ordering(nodes_df: pd.DataFrame) -> list:
+    """
+    Return list of nodeids in the order they were completely solved by
+    the depth-first search algorithm
+    """
+    dfs_ordering = [0]
+    boundary = nodes_df[nodes_df['ParentID'] == 0].sort_values('Alternative', ascending=False).index.to_list()
 
+    # run simulated dfs on tree
+    while len(boundary) > 0:
+        nxt = boundary.pop()
+        dfs_ordering.append(nxt)
+        boundary.extend(nodes_df[nodes_df['ParentID'] == nxt].sort_values('Alternative', ascending=False).index.to_list())
+
+    return dfs_ordering
+
+def plot_goodness(nodes_df: pd.DataFrame, axis) -> None:
+    """
+    Plot prediction of tree weight against ground truth
+    nodes_df must have node weight already assigned
+    """
+    if 'NodeWeight' not in nodes_df.columns:
+        raise ValueError
+    if abs(1 - nodes_df[nodes_df['Status'].isin({1, 0, 3})]['NodeWeight'].sum()) >= 10e-6:
+        print(abs(1 - nodes_df[nodes_df['Status'].isin({1, 0, 3})]['NodeWeight'].sum())) 
+
+    dfs_ordering = make_dfs_ordering(nodes_df)
+
+    cumulative = nodes_df.reindex(dfs_ordering).reset_index()
+    cumulative = cumulative[cumulative['Status'].isin({0, 1, 3})]['NodeWeight']\
+        .cumsum()\
+        .reindex(list(range(1, nodes_df.shape[0]+1)), method='ffill')\
+        .fillna(0)
+
+    x = range(1, nodes_df.shape[0] + 1)
+    axis.plot(pd.Series(x) / nodes_df.shape[0], label='Ground Truth')
+    axis.plot(cumulative, label='Uniform weight')
+    axis.legend()
+    
 if __name__ == '__main__':
     ### do testing here
+    import time
 
-    tree = './trees/golomb_3.sqlite'
-    nodes_df = to_df(tree, 'nodes').set_index('NodeID')
+    start = time.time()
+    tree = './trees/golomb_{}.sqlite'
+    fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(20, 20))
+    for i in range(3, 11):
+        ax = axs[(i - 3) // 3][(i - 3) % 3]
+        nodes_df = to_df(tree.format(i), 'nodes').set_index('NodeID')
+        assign_weights(nodes_df, uniform_scheme)
+        ax.title.set_text(f'golomb_{i}_nodes={nodes_df.shape[0]}')
+        plot_goodness(nodes_df, axis=ax)
+    
+    print("Time taken: {}".format(time.time() - start))
+    plt.show()
+
+    # tree = './trees/golomb_11.sqlite'
+    # nodes_df = to_df(tree, 'nodes').set_index('NodeID')
+    # assign_weights(nodes_df, uniform_scheme)
+    # fig, ax = plt.subplots(nrows=1, ncols=1, figisze=(6,6))
+    # ax.title.set_text(f'golomb_11_nodes={nodes_df.shape[0]}')
+    # plot_goodness(nodes_df, axis=ax)
+    # plt.show()
     
 
 
