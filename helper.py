6414d5ff8551be91e912d271c212d0a8482b02c7
LongDang <hoanglongdang2001@gmail.com>
2021-02-10 17:45:30 +1100

    Add some assertions to ensure correctness and move functions around a bit.

--- helper.py
+++ helper.py
@@ -3,9 +3,13 @@ helper file
 """
 
 import sqlite3
+import json
 import pandas as pd
+import matplotlib.pyplot as plt
+
 import unittest
 
+EPSILON = 10e-7
 
 def to_df(file: str, table: str) -> pd.DataFrame:
     
@@ -25,6 +29,92 @@ def to_df(file: str, table: str) -> pd.DataFrame:
     query = 'SELECT * FROM {}'.format(table.upper())
     return pd.read_sql_query(query, conn)
 
+def parse_info_string(node_info: str) -> dict:
+    """
+    Parse node info string to dict of variables and their domains.
+    Domains can be ranges, or other collectors that support a function len() for caculating their size.
+    Domains must not be empty (otherwise the node will have been pruned)
+
+    Returns:
+        - {variable {str}: domain{range} }
+    """
+    x = node_info.replace("\n", "").replace("\t", "") # replace new lines and tabs
+    x = x.strip('{"domains": ').strip('"}') # strip JSON notation
+    x = x.split(';')[:-1]
+
+    info_dict = {}
+    for i in range(len(x)):
+        x[i] = x[i].strip("var ")
+        domain, varname = x[i].split(': ')
+        if '..' in domain:
+            start, end = (int(num) for num in domain.split('..'))
+            info_dict[varname] = range(start, end + 1)
+        else:
+            varname, value = varname.split(" = ")
+            if str.isdigit(value):
+                info_dict[varname] = range(int(value), int(value) + 1)
+            else:
+                info_dict[varname] = value
+            
+            try:
+                assert len(info_dict[varname]) > 1
+            except AssertionError as e:
+                raise e
+            except ValueError:
+                raise ValueError("Cannot get size of domain of a variable with len!")
+
+    return info_dict
+
+def get_domain_size(node_info: str) -> int:
+    """
+    Get the size of all possible configurations possible for a node
+    """
+    
+    info_dict = parse_info_string(node_info)
+    size = 1
+    for domain_range in info_dict.values():
+        size *= len(domain_range)
+
+    return size
+
+def make_dfs_ordering(nodes_df: pd.DataFrame) -> list:
+    """
+    Return list of nodeids in the order they were completely solved by
+    the depth-first search algorithm
+    """
+    dfs_ordering = [0]
+    boundary = nodes_df[nodes_df['ParentID'] == 0].sort_values('Alternative', ascending=False).index.to_list()
+
+    # run simulated dfs on tree
+    while len(boundary) > 0:
+        nxt = boundary.pop()
+        dfs_ordering.append(nxt)
+        boundary.extend(nodes_df[nodes_df['ParentID'] == nxt].sort_values('Alternative', ascending=False).index.to_list())
+
+    return dfs_ordering
+
+def plot_goodness(nodes_df: pd.DataFrame, axis) -> None:
+    """
+    Plot prediction of tree weight against ground truth
+    nodes_df must have node weight already assigned
+    """
+    if 'NodeWeight' not in nodes_df.columns:
+        raise ValueError
+    if abs(1 - nodes_df[nodes_df['Status'].isin({1, 0, 3})]['NodeWeight'].sum()) >= 10e-6:
+        print(abs(1 - nodes_df[nodes_df['Status'].isin({1, 0, 3})]['NodeWeight'].sum())) 
+
+    dfs_ordering = make_dfs_ordering(nodes_df)
+
+    cumulative = nodes_df.reindex(dfs_ordering).reset_index()
+    cumulative = cumulative[cumulative['Status'].isin({0, 1, 3})]['NodeWeight']\
+        .cumsum()\
+        .reindex(list(range(1, nodes_df.shape[0]+1)), method='ffill')\
+        .fillna(0)
+
+    x = range(1, nodes_df.shape[0] + 1)
+    axis.plot(pd.Series(x) / nodes_df.shape[0], label='Ground Truth')
+    axis.plot(cumulative, label='Uniform weight')
+    axis.legend()
 
 ###################
 #### UNIT TEST ####
@@ -33,7 +123,7 @@ def to_df(file: str, table: str) -> pd.DataFrame:
 class TestHelperMethods(unittest.TestCase):
 
     def test_todict(self):
-        k = to_df('./trees/golomb_4.sqlite', 'nodes')
+        k = to_df('./trees/golomb_4_1.sqlite', 'nodes')
         self.assertEqual(k.loc[0, 'NodeID'], 0)
         self.assertEqual(k.shape[0], 36)
         self.assertTrue(all([k.loc[idx, 'NodeID'] == idx for idx in range(k.shape[0])]))
@@ -43,6 +133,29 @@ class TestHelperMethods(unittest.TestCase):
             set(k.columns)
         )
 
+    def test_parseinfostring(self):
+        test_infostring = \
+        '''
+        {
+            "domains": "var 5..25: X_INTRODUCED_4_;
+        var 13..79: VAR_2;
+        var int: X_INTRODUCED_1_ = 0;
+        var 30..38: VAR_3;
+        var 3..23: X_INTRODUCED_2_;
+        "
+        }
+        '''
+        info_dict = parse_info_string(test_infostring)
+        self.assertEqual(len(info_dict), 5)
+        self.assertEqual(set(info_dict.items()), 
+                        set([('X_INTRODUCED_1_', range(0, 1)), 
+                             ('VAR_2', range(13, 80)),
+                             ('VAR_3', range(30, 39)),
+                             ('X_INTRODUCED_2_', range(3, 24)),
+                             ('X_INTRODUCED_4_', range(5, 26))]))
+    
+    def test_makedfsordering(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
--- tree_weight.py
+++ tree_weight.py
@@ -6,13 +6,34 @@ Implements an offline tree-weight estimation or a constrained search tree
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from helper import to_df
+from helper import to_df, make_dfs_ordering, plot_goodness, EPSILON
 
-def uniform_scheme(node_id: int, nodes_df: pd.DataFrame) -> float:
-    """ Assign uniform weights """
+from typing import List
+
+###########################
+#### WEIGHTING SCHEMES ####
+###########################
+
+
+def uniform_scheme(node_id: int, nodes_df: pd.DataFrame) -> List[float]:
+    """ Assign uniform weights for a node's children """
     return [1 / nodes_df.loc[node_id, 'NKids'] for i in range(nodes_df.loc[node_id, 'NKids'])]
 
-def assign_weights(nodes_df: pd.DataFrame, weight_scheme: 'function') -> None:
+def domain_weight_scheme(node_id: int, nodes_df: pd.DataFrame, info_df: pd.DataFrame=None) -> List[float]:
+    """ Assign weights based on domain's sizes for a node's children """
+
+    if info_df is None:
+        raise ValueError("No information on nodes given!")
+
+    children_df = nodes_df[nodes_df['ParentID'] == node_id]
+    children_df['DomainSize'] = info_df.loc[children_df.index, 'Info'].apply(get_domain_size)
+    return (children_df['DomainSize'] / children_df['DomainSize'].sum()).to_list()
+
+#############################
+#### ASSIGNMENT FUNCTION ####
+#############################
+
+def assign_weights(nodes_df: pd.DataFrame, weight_scheme: 'function', **kwargs) -> None:
     """
     Assigns weight = 1 to root and propogates that weight down using a weighting scheme
     
@@ -20,63 +41,27 @@ def assign_weights(nodes_df: pd.DataFrame, weight_scheme: 'function') -> None:
         - nodes_df: dataframe of nodes, indexed by nodes_id
         - weight_scheme: function that takes in the current node_id
                          and outputs a list of weights for its children
+        - kwargs: keyword arguments required for the weight scheme
     """
     
     nodes_df['NodeWeight'] = 0
     nodes_df.loc[0, 'NodeWeight'] = 1 # root node has weight 1
-    i = 0
 
     # propogate weights down
-    while i < nodes_df.shape[0] - 1:
+    for i in nodes_df[nodes_df['NKids'] > 0].index:
 
         par_weight = nodes_df.loc[i, 'NodeWeight']
         num_kids = nodes_df.loc[i, 'NKids']
         if num_kids > 0:
-            weights = uniform_scheme(i, nodes_df)
+            weights = weight_scheme(i, nodes_df, **kwargs)
             assert len(weights) == num_kids
+            assert abs(1 - sum(weights)) < EPSILON
             nodes_df.loc[nodes_df['ParentID'] == i, 'NodeWeight'] = par_weight * \
                                    nodes_df[nodes_df['ParentID'] == i]['Alternative'].apply(lambda x: weights[x]) 
-        i += 1
-
-def make_dfs_ordering(nodes_df: pd.DataFrame) -> list:
-    """
-    Return list of nodeids in the order they were completely solved by
-    the depth-first search algorithm
-    """
-    dfs_ordering = [0]
-    boundary = nodes_df[nodes_df['ParentID'] == 0].sort_values('Alternative', ascending=False).index.to_list()
-
-    # run simulated dfs on tree
-    while len(boundary) > 0:
-        nxt = boundary.pop()
-        dfs_ordering.append(nxt)
-        boundary.extend(nodes_df[nodes_df['ParentID'] == nxt].sort_values('Alternative', ascending=False).index.to_list())
-
-    return dfs_ordering
-
-def plot_goodness(nodes_df: pd.DataFrame, axis) -> None:
-    """
-    Plot prediction of tree weight against ground truth
-    nodes_df must have node weight already assigned
-    """
-    if 'NodeWeight' not in nodes_df.columns:
-        raise ValueError
-    if abs(1 - nodes_df[nodes_df['Status'].isin({1, 0, 3})]['NodeWeight'].sum()) >= 10e-6:
-        print(abs(1 - nodes_df[nodes_df['Status'].isin({1, 0, 3})]['NodeWeight'].sum())) 
-
-    dfs_ordering = make_dfs_ordering(nodes_df)
-
-    cumulative = nodes_df.reindex(dfs_ordering).reset_index()
-    cumulative = cumulative[cumulative['Status'].isin({0, 1, 3})]['NodeWeight']\
-        .cumsum()\
-        .reindex(list(range(1, nodes_df.shape[0]+1)), method='ffill')\
-        .fillna(0)
-
-    x = range(1, nodes_df.shape[0] + 1)
-    axis.plot(pd.Series(x) / nodes_df.shape[0], label='Ground Truth')
-    axis.plot(cumulative, label='Uniform weight')
-    axis.legend()
+            assert abs(nodes_df.loc[nodes_df['ParentID'] == i, 'NodeWeight'].sum() - par_weight) < EPSILON
     
+    assert abs(nodes_df.loc[nodes_df['Status'].isin({0, 1, 3}), 'NodeWeight'].sum() - 1) < EPSILON
+
 if __name__ == '__main__':
     ### do testing here
     import time
@@ -84,7 +69,7 @@ if __name__ == '__main__':
     start = time.time()
     tree = './trees/golomb_{}.sqlite'
     fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(20, 20))
-    for i in range(3, 11):
+    for i in range(3, 8):
         ax = axs[(i - 3) // 3][(i - 3) % 3]
         nodes_df = to_df(tree.format(i), 'nodes').set_index('NodeID')
         assign_weights(nodes_df, uniform_scheme)
@@ -93,14 +78,6 @@ if __name__ == '__main__':
     
     print("Time taken: {}".format(time.time() - start))
     plt.show()
-
-    # tree = './trees/golomb_11.sqlite'
-    # nodes_df = to_df(tree, 'nodes').set_index('NodeID')
-    # assign_weights(nodes_df, uniform_scheme)
-    # fig, ax = plt.subplots(nrows=1, ncols=1, figisze=(6,6))
-    # ax.title.set_text(f'golomb_11_nodes={nodes_df.shape[0]}')
-    # plot_goodness(nodes_df, axis=ax)
-    # plt.show()
     
 
 
