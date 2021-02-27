16fc59fbcaec33aede6c7c740865bdc9eaebc84b
LongDang <hoanglongdang2001@gmail.com>
2021-02-27 21:02:21 +1100

    Add benchmark models and graphs, change helper.py to resolve bugs

--- helper.py
+++ helper.py
@@ -5,11 +5,32 @@ helper file
 import sqlite3
 import json
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 import unittest
 
 EPSILON = 10e-7
+
+class DomainRange:
+
+    def __init__(self, values: list):
+        
+        assert isinstance(values, list)
+        assert len(values) > 0
+
+        self.values = values # store pass in list of values
+
+    def __len__(self):
+        
+        length = 0
+        for value in self.values:
+            try:
+                length += len(value)
+            except ValueError:
+                length += 1 # assume value is a single value
+        
+        return length
 
 def to_df(file: str, table: str) -> pd.DataFrame:
     
@@ -27,7 +48,9 @@ def to_df(file: str, table: str) -> pd.DataFrame:
     assert table.upper() in  {'NODES', 'INFO', 'BOOKMARKS', 'NOGOODS'}
     conn = sqlite3.connect(file)
     query = 'SELECT * FROM {}'.format(table.upper())
-    return pd.read_sql_query(query, conn)
+    result_df = pd.read_sql_query(query, conn)
+    conn.close()
+    return result_df
 
 def parse_info_string(node_info: str) -> dict:
     """
@@ -44,24 +67,35 @@ def parse_info_string(node_info: str) -> dict:
 
     info_dict = {}
     for i in range(len(x)):
-        x[i] = x[i].strip("var ")
+        x[i] = x[i].replace("var ", "")
         domain, varname = x[i].split(': ')
-        if '..' in domain:
-            start, end = (int(num) for num in domain.split('..'))
-            info_dict[varname] = range(start, end + 1)
+        
+        if 'array_union' in domain:
+            domain = domain.strip(' array_union([])').strip('])').split(',')
         else:
+            domain = [domain.strip()]
+        
+        values = []
+        for value_range in domain:
+            if '..' in value_range:
+                start, end = (int(num) for num in value_range.split('..'))
+                values.append(range(start, end + 1))
+
+        if ' = ' in varname:
             varname, value = varname.split(" = ")
             if str.isdigit(value):
-                info_dict[varname] = range(int(value), int(value) + 1)
+                values.append(range(int(value), int(value) + 1))
             else:
-                info_dict[varname] = value
-            
-            try:
-                assert len(info_dict[varname]) > 1
-            except AssertionError as e:
-                raise e
-            except ValueError:
-                raise ValueError("Cannot get size of domain of a variable with len!")
+                values.append(value)
+        
+        info_dict[varname] = DomainRange(values)
+
+        try:
+            assert len(info_dict[varname]) >= 1
+        except AssertionError as e:
+            raise e
+        except ValueError:
+            raise ValueError("Cannot get size of domain of a variable with len!")
 
     return info_dict
 
@@ -72,49 +106,46 @@ def get_domain_size(node_info: str) -> int:
     
     info_dict = parse_info_string(node_info)
     size = 1
-    for domain_range in info_dict.values():
-        size *= len(domain_range)
+    for domain in info_dict.values():
+        size *= len(domain)
 
     return size
 
 def make_dfs_ordering(nodes_df: pd.DataFrame) -> list:
     """
     Return list of nodeids in the order they were completely solved by
-    the depth-first search algorithm
+    the depth-first search algorithm.
+    We ignored restart nodes.
     """
     dfs_ordering = [0]
-    boundary = nodes_df[nodes_df['ParentID'] == 0].sort_values('Alternative', ascending=False).index.to_list()
+    boundary = nodes_df[(nodes_df['ParentID'] == 0) & (nodes_df['Status'] != 3)]\
+                .sort_values('Alternative', ascending=False).index.to_list()
 
     # run simulated dfs on tree
     while len(boundary) > 0:
         nxt = boundary.pop()
         dfs_ordering.append(nxt)
-        boundary.extend(nodes_df[nodes_df['ParentID'] == nxt].sort_values('Alternative', ascending=False).index.to_list())
+        boundary.extend(nodes_df[(nodes_df['ParentID'] == nxt) & (nodes_df['Status'] != 3)]\
+                             .sort_values('Alternative', ascending=False).index.to_list())
 
+    assert set(dfs_ordering) == (set(nodes_df.index) - set(nodes_df[nodes_df['Status'] == 3].index))
     return dfs_ordering
 
-def plot_goodness(nodes_df: pd.DataFrame, axis) -> None:
+def get_cum_weights(nodes_df: pd.DataFrame, ordering: list) -> pd.Series:
     """
-    Plot prediction of tree weight against ground truth
-    nodes_df must have node weight already assigned
+    Calculate cumulative sum according to an ordering
     """
-    if 'NodeWeight' not in nodes_df.columns:
-        raise ValueError
-    if abs(1 - nodes_df[nodes_df['Status'].isin({1, 0, 3})]['NodeWeight'].sum()) >= 10e-6:
-        print(abs(1 - nodes_df[nodes_df['Status'].isin({1, 0, 3})]['NodeWeight'].sum())) 
+    if set(ordering) != (set(nodes_df.index) - set(nodes_df[nodes_df['Status'] == 3].index)):
+        raise ValueError("Ordering is not a correct ordering of nodes. Some nodes are missing or redundant")
 
-    dfs_ordering = make_dfs_ordering(nodes_df)
+    cumulative = nodes_df.reindex(ordering)
+    cumsum = cumulative[cumulative['Status'].isin({0, 1})]['NodeWeight']\
+                         .cumsum()\
+                         .reindex(ordering)\
+                         .reset_index(drop=True)\
+                         .fillna(method='ffill').fillna(0)  
 
-    cumulative = nodes_df.reindex(dfs_ordering).reset_index()
-    cumulative = cumulative[cumulative['Status'].isin({0, 1, 3})]['NodeWeight']\
-        .cumsum()\
-        .reindex(list(range(1, nodes_df.shape[0]+1)), method='ffill')\
-        .fillna(0)
-
-    x = range(1, nodes_df.shape[0] + 1)
-    axis.plot(pd.Series(x) / nodes_df.shape[0], label='Ground Truth')
-    axis.plot(cumulative, label='Uniform weight')
-    axis.legend()
+    return cumsum
 
 ###################
 #### UNIT TEST ####
@@ -123,7 +154,7 @@ def plot_goodness(nodes_df: pd.DataFrame, axis) -> None:
 class TestHelperMethods(unittest.TestCase):
 
     def test_todict(self):
-        k = to_df('./trees/golomb_4_1.sqlite', 'nodes')
+        k = to_df('./benchmark_models/golomb/trees/04.sqlite', 'nodes')
         self.assertEqual(k.loc[0, 'NodeID'], 0)
         self.assertEqual(k.shape[0], 36)
         self.assertTrue(all([k.loc[idx, 'NodeID'] == idx for idx in range(k.shape[0])]))
@@ -140,22 +171,73 @@ class TestHelperMethods(unittest.TestCase):
             "domains": "var 5..25: X_INTRODUCED_4_;
         var 13..79: VAR_2;
         var int: X_INTRODUCED_1_ = 0;
-        var 30..38: VAR_3;
+        var array_union([30..38, 50..57]): VAR_3;
         var 3..23: X_INTRODUCED_2_;
         "
         }
         '''
         info_dict = parse_info_string(test_infostring)
         self.assertEqual(len(info_dict), 5)
-        self.assertEqual(set(info_dict.items()), 
-                        set([('X_INTRODUCED_1_', range(0, 1)), 
-                             ('VAR_2', range(13, 80)),
-                             ('VAR_3', range(30, 39)),
-                             ('X_INTRODUCED_2_', range(3, 24)),
-                             ('X_INTRODUCED_4_', range(5, 26))]))
+        self.assertEqual(set(info_dict.keys()), {'VAR_2', 'VAR_3', 'X_INTRODUCED_1_', 'X_INTRODUCED_2_', 'X_INTRODUCED_4_'})
+        self.assertEqual(set(info_dict['VAR_2'].values), set([range(13, 80)]))
+        self.assertEqual(set(info_dict['VAR_3'].values), set([range(30, 39), range(50, 58)]))
+        self.assertEqual(set(info_dict['X_INTRODUCED_1_'].values), set([range(0, 1)]))
+        self.assertEqual(set(info_dict['X_INTRODUCED_2_'].values), set([range(3, 24)]))
+        self.assertEqual(set(info_dict['X_INTRODUCED_4_'].values), set([range(5, 26)]))
+        self.assertEqual(sum([len(x) for x in info_dict.values()]), 1 + 9 + 8 + 67 + 21 + 21)
     
     def test_makedfsordering(self):
-        pass
+        """
+        Using this random tree:
+                   0
+                  / \   \
+                 1   2   3  
+               / /\  /\  /\
+              4 5 6 7  8 9 10
+                           /\ \
+                         11 12 13
+
+        Where status of the leaf nodes are: {4: 0, 5: 1, 6: 1, 7: 0, 8: 3, 9: 1, 11: 1, 12: 3, 13: 3}
+        """
+
+        test_df = pd.DataFrame({
+            'NodeID': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+            'ParentID': [-1, 0, 0, 0, 1, 1, 1, 2, 2, 3, 3, 10, 10, 10],
+            'Alternative': [-1, 0, 1, 2, 0, 1, 2, 0, 1, 0, 1, 0, 1, 2],
+            'NKids': [3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
+            'Status': [2, 2, 2, 2, 0, 1, 1, 0, 3, 1, 2, 1, 3, 3]
+        }) 
+
+        ordering = make_dfs_ordering(test_df)
+        self.assertEqual(ordering, [0, 1, 4, 5, 6, 2, 7, 3, 9, 10, 11])
+
+    def test_getcumweight(self):
+        """
+        Using this random tree:
+                   0
+                  / \   \
+                 1   2   3  
+               / /\  /\  /\
+              4 5 6 7  8 9 10
+                           /\ \
+                         11 12 13
+
+        Where status of the leaf nodes are: {4: 0, 5: 1, 6: 1, 7: 0, 8: 3, 9: 1, 11: 1, 12: 3, 13: 3}
+        """
+        
+        test_df = pd.DataFrame({
+            'NodeID': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+            'ParentID': [-1, 0, 0, 0, 1, 1, 1, 2, 2, 3, 3, 10, 10, 10],
+            'Alternative': [-1, 0, 1, 2, 0, 1, 2, 0, 1, 0, 1, 0, 1, 2],
+            'NKids': [3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
+            'Status': [2, 2, 2, 2, 0, 1, 1, 0, 3, 1, 2, 1, 3, 3],
+            'NodeWeight': [1, 1/3, 1/3, 1/3, 1/9, 1/9, 1/9, 1/3, 0, 1/6, 1/6, 1/6, 0, 0]
+        }) 
+
+        ordering = make_dfs_ordering(test_df)
+        cumsum = get_cum_weights(test_df, ordering)
+
+        self.assertTrue(all(np.abs(cumsum.values - pd.Series([0, 0, 1/9, 2/9, 1/3, 1/3, 2/3, 2/3, 5/6, 5/6, 1])) < EPSILON))
 
 if __name__ == '__main__':
     unittest.main()
--- tree_weight.py
+++ tree_weight.py
@@ -1,12 +1,11 @@
 """
 Implements an offline tree-weight estimation or a constrained search tree
-
-4. Measure weight as we go along node_id and compare it with actual number of nodes we eventually visit
 """
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from helper import to_df, make_dfs_ordering, plot_goodness, EPSILON
+from helper import to_df, make_dfs_ordering, get_domain_size, EPSILON
 
 from typing import List
 
@@ -17,17 +16,28 @@ from typing import List
 
 def uniform_scheme(node_id: int, nodes_df: pd.DataFrame) -> List[float]:
     """ Assign uniform weights for a node's children """
-    return [1 / nodes_df.loc[node_id, 'NKids'] for i in range(nodes_df.loc[node_id, 'NKids'])]
 
-def domain_weight_scheme(node_id: int, nodes_df: pd.DataFrame, info_df: pd.DataFrame=None) -> List[float]:
-    """ Assign weights based on domain's sizes for a node's children """
+    if nodes_df['Status'].isin([3]).sum() > 0:
+        raise ValueError('Nodes_df must consist only of valid nodes')
+    num_kids = nodes_df[nodes_df['ParentID'].isin([node_id])].shape[0]
+    weights = [1 / num_kids] * num_kids
+    return weights
+
+def domain_weight_scheme(node_id: int, nodes_df: pd.DataFrame, info_df: pd.DataFrame=None,
+                            no_good_no_domain: bool=False) -> List[float]:
+    """ Assign weights based on domain's sizes for a node's children, ignoring restart nodes """
 
     if info_df is None:
         raise ValueError("No information on nodes given!")
+    elif info_df.index.name != 'NodeID':
+        raise ValueError("Info dataframe must be indexed by node id")
+    elif nodes_df['Status'].isin([3]).sum() > 0:
+        raise ValueError('Nodes_df must consist only of valid nodes')
 
-    children_df = nodes_df[nodes_df['ParentID'] == node_id]
-    children_df['DomainSize'] = info_df.loc[children_df.index, 'Info'].apply(get_domain_size)
-    return (children_df['DomainSize'] / children_df['DomainSize'].sum()).to_list()
+
+    kids = nodes_df[nodes_df['ParentID'] == node_id]
+    domains = info_df.loc[kids.index, 'Info'].apply(get_domain_size)
+    return (domains / domains.sum()).to_list()
 
 #############################
 #### ASSIGNMENT FUNCTION ####
@@ -35,7 +45,8 @@ def domain_weight_scheme(node_id: int, nodes_df: pd.DataFrame, info_df: pd.DataF
 
 def assign_weights(nodes_df: pd.DataFrame, weight_scheme: 'function', **kwargs) -> None:
     """
-    Assigns weight = 1 to root and propogates that weight down using a weighting scheme
+    Assigns weight = 1 to root and propogates that weight down using a weighting scheme.
+    Nodes which are backjumped over and pruned are ignored.
     
     Parameter:
         - nodes_df: dataframe of nodes, indexed by nodes_id
@@ -45,39 +56,51 @@ def assign_weights(nodes_df: pd.DataFrame, weight_scheme: 'function', **kwargs) 
     """
     
     nodes_df['NodeWeight'] = 0
-    nodes_df.loc[0, 'NodeWeight'] = 1 # root node has weight 1
+    invalid_status = [3] # pruned nodes that are jumped over without being considered
+    valid_df = pd.DataFrame.copy(nodes_df[~nodes_df['Status'].isin(invalid_status)])
+    valid_df.loc[0, 'NodeWeight'] = 1 # root node has weight 1
 
     # propogate weights down
-    for i in nodes_df[nodes_df['NKids'] > 0].index:
+    for i in valid_df[valid_df['NKids'] > 0].index:
 
-        par_weight = nodes_df.loc[i, 'NodeWeight']
-        num_kids = nodes_df.loc[i, 'NKids']
-        if num_kids > 0:
-            weights = weight_scheme(i, nodes_df, **kwargs)
-            assert len(weights) == num_kids
-            assert abs(1 - sum(weights)) < EPSILON
-            nodes_df.loc[nodes_df['ParentID'] == i, 'NodeWeight'] = par_weight * \
-                                   nodes_df[nodes_df['ParentID'] == i]['Alternative'].apply(lambda x: weights[x]) 
-            assert abs(nodes_df.loc[nodes_df['ParentID'] == i, 'NodeWeight'].sum() - par_weight) < EPSILON
-    
-    assert abs(nodes_df.loc[nodes_df['Status'].isin({0, 1, 3}), 'NodeWeight'].sum() - 1) < EPSILON
+        par_weight = valid_df.loc[i, 'NodeWeight']
+        children = valid_df[valid_df['ParentID'].isin([i])]
+        
+        if children.shape[0] == 0:
+            continue
+
+        weights = weight_scheme(i, valid_df, **kwargs)
+        assert len(weights) == children.shape[0]
+        assert abs(1 - sum(weights)) < EPSILON
+        
+        valid_df.loc[children.index, 'NodeWeight'] = par_weight * np.array(weights) 
+        assert abs(valid_df.loc[valid_df['ParentID'] == i, 'NodeWeight'].sum() - par_weight) < EPSILON
+
+    assert abs(valid_df.loc[valid_df['Status'].isin({0, 1, 3}), 'NodeWeight'].sum() - 1) < EPSILON
+
+    # assign valid back to nodes, default invalid status nodes' weights to 0
+    # this preserves the postconditions, as sum of children is unchanged
+    nodes_df.loc[valid_df.index, 'NodeWeight'] = valid_df['NodeWeight']
+    nodes_df['NodeWeight'].fillna(0, inplace=True)
 
 if __name__ == '__main__':
-    ### do testing here
-    import time
+    # ### do testing here
+    # import time
+    # import pathlib
 
-    start = time.time()
-    tree = './trees/golomb_{}.sqlite'
-    fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(20, 20))
-    for i in range(3, 8):
-        ax = axs[(i - 3) // 3][(i - 3) % 3]
-        nodes_df = to_df(tree.format(i), 'nodes').set_index('NodeID')
-        assign_weights(nodes_df, uniform_scheme)
-        ax.title.set_text(f'golomb_{i}_nodes={nodes_df.shape[0]}')
-        plot_goodness(nodes_df, axis=ax)
-    
-    print("Time taken: {}".format(time.time() - start))
-    plt.show()
-    
+    # start = time.time()
+    # fig, axs = plt.subplots(nrows=3, ncols=9, figsize=(9, 27))
 
+    # tree_files = [tree]
+
+    # for i in range(3 * 9):
+    #     ax = axs[i // 3][i % 3]
+    #     nodes_df = to_df(tree.format(i), 'nodes').set_index('NodeID')
+    #     assign_weights(nodes_df, uniform_scheme)
+    #     ax.title.set_text(f'golomb_{i}_nodes={nodes_df.shape[0]}')
+    #     plot_goodness(nodes_df, axis=ax)
+    
+    # print("Time taken: {}".format(time.time() - start))
+    # plt.show()
+    pass
 
